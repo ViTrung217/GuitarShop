32b5f0b2a5f445eab52b63619391391804b24784
ViTrung217 <vhtrung2004@gmail.com>
2026-01-20 16:33:07 +0700

    hoàn thiện các chức năng

---
+++ GuitarShop/src/main/java/com/guitarshop/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.guitarshop.repository;
+
+import com.guitarshop.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
